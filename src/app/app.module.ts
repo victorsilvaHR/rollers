89e0b166abddf327b58f80564f2fba8ab5fd5149
Neutron21 <tanerkratos@hotmail.com>
2024-10-25 12:53:51 -0600

    agregando footer y favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaginaComponent } from './pagina/pagina.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaginaComponent
+    PaginaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
