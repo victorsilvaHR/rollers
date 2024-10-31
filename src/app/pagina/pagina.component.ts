0007c73685b2365d46c0821fdd1fbd9294e80597
victorsilvaHR <victor.hugo.silva01@gmail.com>
2024-10-31 12:36:09 -0600

    se hace afecto scroll

--- src/app/pagina/pagina.component.ts
+++ src/app/pagina/pagina.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-pagina',
@@ -6,15 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagina.component.scss']
 })
 export class PaginaComponent implements OnInit {
-  
+
   isNavbarVisible: boolean = false;
 
-  constructor() { }
-
+  constructor(private renderer: Renderer2) { }
   ngOnInit(): void {
+    this.renderer.listen('window', 'scroll', () => {
+      const gallery = document.querySelector('.grid-container-3') as HTMLElement;
+      if (gallery) {
+        const scrollY = window.scrollY;
+        gallery.style.transform = `translateX(${scrollY * 0.3 - 330}px)`; // -200 px para iniciar más a la izquierda
+      }
+    });
   }
 
   toggleNavbar(): void {
     this.isNavbarVisible = !this.isNavbarVisible;
   }
+
 }
